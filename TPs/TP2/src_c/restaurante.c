167c4529f1decdbba12ddd2aa1f64273a35a2cbd
samuel <samuelteodoro058@gmail.com>
2026-04-22 22:13:10 -0300

    tp2 q2 restaurante c

--- TPs/TP2/src_c/restaurante.c
+++ TPs/TP2/src_c/restaurante.c
@@ -74,11 +74,122 @@ static int split(char* src, char delim, char dest[][MAX_TIPO], int max_partes){
 }
 
 Restaurante* parse_restaurante(char* s){
-    Restaurante* r = (Restaurante*) malloc(sixeof(Restaurante));
+    Restaurante* r = (Restaurante*) malloc(sizeof(Restaurante));
 
     char colunas[12][512];
     int n = 0;
 
     char tmp[1024];
     strncpy(tmp,s,sizeof(tmp)-1);
+    tmp[sizeof(tmp)-1] = '\0';
+
+    char* token = strtok(tmp,",");
+    while (token != NULL && n < 12){
+        strncpy(colunas[n],token,511);
+        colunas[n][511] = '\0';
+        n++;
+        token = strtok(NULL, ",");
+    }
+
+    r->id = atoi(colunas[0]);
+    strncpy(r->nome,   colunas[1], MAX_NOME -1);
+    strncpy(r->cidade, colunas[2], MAX_CIDADE - 1);
+    r->capacidade = atoi(colunas[3]);
+    r->avaliacao  = atof(colunas[4]);
+
+    r->n_tipos_cozinha = split(colunas[5], ';', r->tipos_cozinha, MAX_TIPOS_COZINHA);
+    r->faixa_preco = (int) strlen(colunas[6]);
+
+    char partes_horario[2][32];
+    char hor_tmp[64];
+    strncpy(hor_tmp, colunas[7], sizeof(hor_tmp)-1);
+    char* h1 = strtok(hor_tmp, "-");
+    char* h2 = strtok(NULL, "-");
+    r->horario_abertura   = parse_hora(h1);
+    r->horario_fechamento = parse_hora(h2);
+    r->data_abertura      = parse_data(colunas[8]);
+    r->aberto             = (strcmp(colunas[9], "true") == 0);
+
+    return r;
 }
+
+void formatar_restaurante(Restaurante* r, char* buffer){
+    char tipos_str[256] = "[";
+    for (int i = 0; i<r->n_tipos_cozinha; i++){
+        if(i>0) strcat(tipos_str, ",");
+        strcat(tipos_str, r->tipos_cozinha[i]);
+    }
+    strcat(tipos_str, "]");
+
+    char cifroes[8] = "";
+    for(int i = 0; i < r->faixa_preco; i++) strcat(cifroes, "$");
+
+    char buf_abertura[16], buf_fechamento[16], buf_data[16];
+    formatar_hora(&r->horario_abertura,   buf_abertura);
+    formatar_hora(&r->horario_fechamento, buf_fechamento);
+    formatar_data(&r->data_abertura,      buf_data);
+
+    sprintf(buffer, "[%d ## %s ## %s ## %d ## %.1f ## %s ## %s ## %s-%s ## %s ## %s]",
+        r->id, r->nome, r->cidade, r->capacidade, r->avaliacao,
+        tipos_str, cifroes,
+        buf_abertura, buf_fechamento,
+        buf_data,
+        r->aberto ? "true" : "false");
+}
+
+//colecao restaurante
+typedef struct {
+    int tamanho;
+    Restaurante** restaurantes;
+} Colecao_Restaurantes;
+
+void ler_csv_colecao(Colecao_Restaurantes* colecao, char* path) {
+    FILE* arquivo = fopen(path, "r");
+    if (arquivo == NULL) {
+        fprintf(stderr, "Arquivo não encontrado em: %s\n", path);
+        return;
+    }
+
+    char linha[1024];
+    fgets(linha, sizeof(linha), arquivo);
+
+    colecao->tamanho = 0;
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        int len = strlen(linha);
+        if (len > 0 && linha[len - 1] == '\n') linha[len - 1] = '\0';
+
+        if (strlen(linha) == 0) continue;
+
+        colecao->restaurantes[colecao->tamanho] = parse_restaurante(linha);
+        colecao->tamanho++;
+    }
+
+    fclose(arquivo);
+}
+
+Colecao_Restaurantes* ler_csv() {
+    Colecao_Restaurantes* colecao = (Colecao_Restaurantes*) malloc(sizeof(Colecao_Restaurantes));
+    colecao->restaurantes = (Restaurante**) malloc(1000 * sizeof(Restaurante*));
+    colecao->tamanho = 0;
+    ler_csv_colecao(colecao, "/tmp/restaurantes.csv");
+    return colecao;
+}
+ //main
+ int main(){
+    Colecao_Restaurantes* base = ler_csv();
+    int id_procurado;
+    scanf("%d", &id_procurado);
+
+    while (id_procurado != -1){
+        for(int i = 0; i<base->tamanho; i++){
+            if(base->restaurantes[i]->id == id_procurado){
+                char buffer[1024];
+                formatar_restaurante(base->restaurantes[i], buffer);
+                printf("%s\n", buffer);
+                break;
+            }
+        }
+        scanf("%d", &id_procurado);
+    }
+    return 0;
+ }
